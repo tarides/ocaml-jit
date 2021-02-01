39a0c7071de9ee2baf6b79172e5a1e93de738b10
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-02-01 11:40:59 +0100

    Allow to save temp files in nattop
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- clitop/clitop.ml
+++ clitop/clitop.ml
@@ -1,36 +1,54 @@
-type input = Stdin | In_file of string
+type input =
+  | Stdin
+  | In_file of string
+
+let named f arg_term =
+  Cmdliner.Term.(const f $ arg_term)
 
 let input =
   let doc =
-    "The path to the file containing the toplevel phrase to evaluate, pass \
-     $(b,-) to read from stdin instead"
+    "The path to the file containing the toplevel phrase to evaluate, pass $(b,-) to read from stdin instead"
   in
   let docv = "TOPLEVEL_PHRASE" in
   let input =
-    let parse = function "-" -> `Ok Stdin | s -> `Ok (In_file s) in
+    let parse = function
+      | "-" -> `Ok Stdin
+      | s -> `Ok (In_file s)
+    in
     let print fmt = function
       | Stdin -> Format.fprintf fmt "stdin"
       | In_file s -> Format.fprintf fmt "%s" s
     in
-    (parse, print)
+    parse, print
   in
-  Cmdliner.Arg.(required & pos 0 (some input) None & info ~doc ~docv [])
+  named (fun x -> `Input x)
+    Cmdliner.Arg.(required & pos 0 (some input) None & info ~doc ~docv [])
 
-let run ~eval_phrase input =
+let keep_asm_files =
+  let doc =
+    "Keep temporary files such as the assembler, binary assembler and dll.\n \
+     Only meaningful for native toplevel using the external toolchain."
+  in
+  named (fun x -> `Keep_asm_files x)
+    Cmdliner.Arg.(value & flag & info ~doc ["k"; "keep-asm-files"])
+
+let run ~eval_phrase (`Keep_asm_files keep_asm_files) (`Input input) =
+  Clflags.keep_asm_file := keep_asm_files;
   let phrase =
     match input with
     | Stdin ->
-        let lexbuf = Lexing.from_channel stdin in
-        Parse.toplevel_phrase lexbuf
+      let lexbuf = Lexing.from_channel stdin in
+      Parse.toplevel_phrase lexbuf
     | In_file file ->
-        let ic = open_in file in
-        let lexbuf = Lexing.from_channel ic in
-        Parse.toplevel_phrase lexbuf
+      let ic = open_in file in
+      let lexbuf = Lexing.from_channel ic in
+      Parse.toplevel_phrase lexbuf
   in
   let _ = eval_phrase true Format.std_formatter phrase in
   ()
 
-let term ~eval_phrase = Cmdliner.Term.(const (run ~eval_phrase) $ input)
+let term ~eval_phrase =
+  Cmdliner.Term.(const (run ~eval_phrase) $ keep_asm_files $ input)
 
 let info ~name =
   let doc = "Run the given OCaml toplevel phrase" in
