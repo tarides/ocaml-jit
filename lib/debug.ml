c67b7044b9d1c015c1e3bd658e5ad2f1ec776dd3
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-06-03 14:58:11 +0200

    Strip ppx_deriving out
    This breaks all debugging features, not to merge!
    
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- lib/debug.ml
+++ lib/debug.ml
@@ -16,15 +16,10 @@
 
 open Import
 
-let print_ast asm_program =
-  if !Globals.debug then (
-    Printf.printf "-------- X86 AST --------\n%!";
-    Printf.printf "%s\n%!" (X86_ast_helpers.show_asm_program asm_program))
+let print_ast _ = ()
 
-let print_section_map section_map =
-  if !Globals.debug then (
-    Printf.printf "-------- Sections ---------\n%!";
-    Printf.printf "%s\n%!" (X86_section.Map.show section_map))
+let print_section_map _section_map =
+  ()
 
 let write_bin_file ~filename content =
   let oc = open_out_bin filename in
@@ -48,17 +43,5 @@ let save_text_section ~phrase_name { address; value = text_section } =
     in
     write_bin_file ~filename (Jit_text_section.content text_section)
 
-let print_binary_section_map binary_section_map =
-  if !Globals.debug then (
-    Printf.printf "-------- Binary Sections ---------\n%!";
-    Printf.printf "Relocations:\n%!";
-    String.Map.iter binary_section_map ~f:(fun ~key:section_name ~data:buffer ->
-        Printf.printf "------ Section: %s ------\n%!" section_name;
-        let relocations = X86_emitter.relocations buffer in
-        Printf.printf "%s\n%!"
-          ([%show: X86_emitter.Relocation.t list] relocations));
-    Printf.printf "Labels:\n%!";
-    String.Map.iter binary_section_map ~f:(fun ~key:section_name ~data:buffer ->
-        Printf.printf "------ Section: %s ------\n%!" section_name;
-        let labels = X86_emitter.labels buffer in
-        Printf.printf "%s\n%!" ([%show: X86_emitter.symbol String.Map.t] labels)))
+let print_binary_section_map _binary_section_map =
+  ()
