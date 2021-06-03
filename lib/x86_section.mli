a2c56380de80920345c6fbb99d116923b8f632f8
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-06-03 14:58:11 +0200

    REMOVE: Strip ppx_deriving out
    This breaks all debugging features, not to merge!
    
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- lib/x86_section.mli
+++ lib/x86_section.mli
@@ -23,7 +23,5 @@ val assemble : arch:X86_ast.arch -> t -> X86_emitter.buffer
 module Map : sig
   type t = X86_ast.asm_line list String.Map.t
 
-  val show : t -> string
-
   val from_program : X86_ast.asm_program -> t
 end
