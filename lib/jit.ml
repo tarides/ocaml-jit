35bf6c0b08eeaf11dd3a0ba6282fbc8c7af84809
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-09-08 17:45:01 +0200

    Redefine the backend module so it doesn't have to be exposed in opttoploop
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- lib/jit.ml
+++ lib/jit.ml
@@ -199,6 +199,23 @@ let with_jit_x86 f =
     X86_proc.internal_assembler := ias;
     raise exn
 
+(* Copied from opttoploop.ml *)
+module Backend = struct
+  let symbol_for_global' = Compilenv.symbol_for_global'
+  let closure_symbol = Compilenv.closure_symbol
+
+  let really_import_approx = Import_approx.really_import_approx
+  let import_symbol = Import_approx.import_symbol
+
+  let size_int = Arch.size_int
+  let big_endian = Arch.big_endian
+
+  let max_sensible_number_of_arguments =
+    (* The "-1" is to allow for a potential closure environment parameter. *)
+    Proc.max_arguments_for_tailcalls - 1
+end
+let backend = (module Backend : Backend_intf.S)
+
 let jit_load_body ppf program =
   let open Config in
   let open Opttoploop in
