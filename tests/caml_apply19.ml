1449cd91047ab0042e70d5f78aa6467df22951e7
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-11-17 17:09:06 +0100

    Add a test case for the caml_apply19 bug
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

---
+++ tests/caml_apply19.ml
@@ -0,0 +1,7 @@
+let force_caml_apply19 _p0 _p1 _p2 _p3 _p4 _p5 _p6 _p7 _p8 _p9 _p10 _p11
+      _p12 _p13 _p14 _p15 _p16 _p17 _p18 _p19 = ()
+
+let x : _ -> unit =
+  (Sys.opaque_identity force_caml_apply19)
+    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+;;
